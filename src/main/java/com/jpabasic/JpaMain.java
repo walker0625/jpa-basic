7397125c0b00e1579a2698ce5be3ab4e23dd83ff
전민우 <mw.jeon@lifesemantics.kr>
2022-05-06 17:55:55 +0900

    설명 주석 추가

--- src/main/java/com/jpabasic/JpaMain.java
+++ src/main/java/com/jpabasic/JpaMain.java
@@ -17,27 +17,38 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
 
         try {
+            /*
             List<Member> findMembers = em.createQuery("SELECT m FROM Member AS m", Member.class) // jpql > 객체를 대상으로 쿼리, sql > 테이블을 대상으로 쿼리
                                          .setFirstResult(5).setMaxResults(8) // paging(mysql, oracle 방언에 맞게 sql을 만듬)
                                          .getResultList();
-            for (Member findMember : findMembers) {
-                System.out.println("findMember.getName() = " + findMember.getName());
-            }
-            /*
+            
+            // 비영속
             Member member = new Member();
             member.setId(1L);
             member.setName("minwoo");
 
+            // 영속
+            // (DB 저장 순간이 아님 - 쿼리가 동작하는 시점이 아님)
             em.persist(member);
 
+            // 준영속
+            em.detach(member);
+
+            // 조회 > 스냅샷 생성
             Member findMember = em.find(Member.class, 1L);
+
+            //갱신 > dirty checking(변경 감지)
             findMember.setName("jeon");
+
+            // 삭제
             em.remove(1L);
 */
-            tx.commit();
+
+            tx.commit(); // flush(쿼리가 동작하는 시점)
         } catch (Exception e) {
             tx.rollback();
         } finally {
